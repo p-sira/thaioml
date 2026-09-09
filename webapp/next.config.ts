b9a36a9b01c61812befc619982af03f1957580df
Sira Pornsiriprasert <code@psira.me>
2026-09-09 20:20:29 +0700

    Fix multiple lock file error

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: "/home/psira/Code/web/thaioml",
+  },
 };
 
 export default nextConfig;
