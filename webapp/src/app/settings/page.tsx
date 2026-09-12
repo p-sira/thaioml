709ffc73df4158af3d59dde37b80cc1401e4027d
Sira Pornsiriprasert <code@psira.me>
2026-09-12 18:25:47 +0700

    FMT

--- webapp/src/app/settings/page.tsx
+++ webapp/src/app/settings/page.tsx
@@ -13,10 +13,6 @@ export default async function SettingsPage() {
 
   return (
     <div className="min-h-screen flex flex-col p-4">
-      <div className="mb-8 max-w-5xl w-full mx-auto pt-4">
-        <h1 className="text-2xl font-bold text-foreground">Settings</h1>
-      </div>
-
       <main className="flex justify-center flex-1">
         <UserSettingsClient currentTheme={currentTheme} />
       </main>
